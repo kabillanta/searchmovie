d9725890a6a834e7ddb8895f887e98ea63f4e365
Kabillan T A <kabillan1905@gmail.com>
2025-03-22 21:08:54 +0530

    initial commit

---
+++ movielist/src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import MovieCard from "../components/MovieCard"
+import { useState } from "react"
+
+function Home () {
+
+    const [searchQuery, setSearchQuery] = useState("");
+    const movies = [
+        {id:1,title:"GTA VI", release_date : "2025"},
+        {id:2,title:"GTA V", release_date : "2013"},
+        {id:3,title:"GTA IV", release_date : "2008"},
+        {id:4,title:"GTA SA", release_date : "2004"}
+    ]
+
+
+    const handleSearch = () => {
+        alert(searchQuery)
+    }
+    return (
+    <div className="home">
+        <form onSubmit={handleSearch} className="search-from">
+            <input onChange={(e) => setSearchQuery(e.target.value)} value={searchQuery} type="text" placeholder="Search for Movies ..." className="search-input" />
+            <button type="submit" className="search-btn">Search </button>
+        </form>
+        <div className="movies-grid">
+            {movies.map(movie => 
+            <MovieCard movie={movie} key={movie.id}/>)}
+        </div>
+    </div>
+    )
+}
+
+export default Home 
